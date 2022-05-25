c10b7b58676ccb5eabb2b659591718f7c6e50069
Eusebio Simango <eusebiosimango14@gmail.com>
2022-05-26 01:09:15 +0200

    end api eg.

--- api.py
+++ api.py
@@ -1,12 +1,20 @@
 from flask import Flask 
 from flask_restful import Resource, Api, reqparse
+import pandas as pd
+import ast
 
 app = Flask(__name__)
 api = Api(app)
 
-@app.route('/')
-def index():
-	return "Hello World"
+class Questions(Resource):
+
+	def get(self):
+		data = pd.read_csv('questions.csv')
+		data = data.to_dict() #convert datafram to object/dictionary
+		return [ data ], 200
+
+
+api.add_resource(Questions, '/all')
 
 if __name__=='__main__':
 	app.run(debug=True)
